27ce23bdcae13d857142d72081b6a26a2a777825
mansormd <addbrobusiness@gmail.com>
2026-02-27 17:48:07 +0300

    "main" branch, initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+let name = "Dugsiiye";
+let age = 18;
+let isMarried = true;
+let isWorking = false;
+
+console.log(name);
+console.log(age);
+console.log(isMarried);
+console.log(isWorking);
+
+
